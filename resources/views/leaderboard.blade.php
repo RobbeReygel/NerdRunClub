ca71e365067c50ac9e1a5106bdd0448a9a9532e9
BuildTools <robbereygel97@gmail.com>
2017-10-24 20:55:33 +0200

    Added temporary style to leaderboard

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -2,9 +2,11 @@
 
 @section('content')
     <h2>Leaderboard</h2>
+    <div id="leaderboard">
     <ul>
         @foreach($list as $item)
-            <li>{{ $item }}</li><br>
+            <li><img src="{{ $item->avatar }}" alt="avatar"> {{ $item->first_name }} - {{ $item->totalDistance/1000 }} km</li>
         @endforeach
     </ul>
+    </div>
 @endsection
