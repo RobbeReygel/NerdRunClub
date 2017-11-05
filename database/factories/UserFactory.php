49992a631d4609c769989f5288381420e49fb865
sorenwagemans <soren.wagemans@hotmail.com>
2017-11-05 18:50:51 +0100

    seeding user and activity

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,7 +14,6 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(App\User::class, function (Faker $faker) {
-    static $password;
 
     return [
         'strava_id' => mt_rand(10000000, 99999999),
@@ -26,3 +25,22 @@ $factory->define(App\User::class, function (Faker $faker) {
         'token' => "7de8ff22af9c5c43a5ddcf03b1e8b1aab30b2d8f",
     ];
 });
+
+$factory->define(App\Activity::class, function (Faker $faker) {
+
+    $date = $faker->date($format = 'Y-m-d', $max = 'now');
+    $t = "T";
+    $time = $faker->time($format = 'H:i:s', $max = 'now');
+    $z = "Z";
+    $startDate = $date . $t . $time . $z;
+
+    return [
+        'activityId' => mt_rand(1000000000, 1999999999),
+        'user_id' => $faker->numberBetween($min = 1, $max = 20),
+        'name' => $faker->randomElement($array = array ('Ochtend run','Avondloop','Middag run')),
+        'distance' => $faker->numberBetween($min = 100, $max = 50000),
+        'moving_time' => $faker->numberBetween($min = 100, $max = 50000),
+        'start_date' => $startDate
+    ];
+});
+
