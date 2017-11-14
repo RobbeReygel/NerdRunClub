c9610692fc9a6cf597799e8c3cf680b4b79f2c5d
Pieterjan Van Saet <hybridfox2@gmail.com>
2017-11-14 22:26:55 +0100

    TODO: style medals

--- resources/views/medals.blade.php
+++ resources/views/medals.blade.php
@@ -2,5 +2,10 @@
 
 @section('content')
     <h2>Your medals</h2>
+    <ul>
+        @foreach(Auth::user()->medals as $medal)
+            <li>{{ $medal->type }} - {{ $medal->short_name }}</li>
+        @endforeach
+    </ul>
 @endsection
 
