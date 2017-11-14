b5e82d70319ab1aec50b3bd696a9ca36a6f4ba7c
sorenwagemans <soren.wagemans@hotmail.com>
2017-11-14 11:53:37 +0100

    added layout to index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,29 @@
-@extends ('layout.app')
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
+    <title>Nerd Running Club</title>
 
-@endsection
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
+    <script src="https://use.fontawesome.com/ef9fae15ba.js"></script>
+</head>
+<body>
 
+<div class="bgContainer">
+    <section class="indexContent">
+    <h1>Deze tekst wordt een mooie motivatie slogan</h1>
+    <div class=""> <a href="{{ route('auth.redirect') }}" class="btn btn-sm animated-button victoria-two">Login</a> </div>
+    </section>
+</div>
+<nav class="textLogo">
+    <a class="navbar-brand" href="/">NerdRunClub</a>
+</nav>
+</body>
+</html>
