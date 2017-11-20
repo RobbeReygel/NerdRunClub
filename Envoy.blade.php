84639747ae7e993a55e7550ed43a2b02f8d41f17
Pieterjan Van Saet <hybridfox2@gmail.com>
2017-11-20 11:36:58 +0100

    Envoy

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,7 +1,9 @@
-@server(['web' => ])
+@servers(['web' => 'deployrobot@172.104.132.227'])
 
-@task('deploy')
-    cd /var/www/master
+@task('deploy', ['on' => 'web'])
+    exec ssh-agent bash
+    ssh-add ~/.ssh/git
+    cd /home/nerdrunners
     git pull origin master
     php artisan migrate:refresh
 @endtask
