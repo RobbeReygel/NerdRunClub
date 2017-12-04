dab1864e133791e5722368e596a287f910190b6c
BuildTools <robbereygel97@gmail.com>
2017-12-04 15:11:11 +0100

    leaderboard css edit

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -6,19 +6,19 @@
     <ul>
         @foreach($list as $item)
         @if ($i == 1)
-            <li style="border: 5px solid gold">
+            <li style="border-left: 6px solid gold">
                 <h3><span class="fa fa-trophy" style="color:gold"></span> {{ $i++ }}</h3>
                 <img src="{{ $item->avatar }}" alt="avatar">
                 <span>{{ $item->first_name }} - {{ number_format($item->totalDistanceWeekly/1000) }} km</span>
             </li>
         @elseif ($i == 2)
-            <li style="border: 5px solid silver">
+            <li style="border-left: 6px solid silver">
                 <h3><span class="fa fa-trophy" style="color:silver"></span> {{ $i++ }}</h3>
                 <img src="{{ $item->avatar }}" alt="avatar">
                 <span>{{ $item->first_name }} - {{ number_format($item->totalDistanceWeekly/1000) }} km</span>
             </li>
         @elseif ($i == 3)
-            <li style="border: 5px solid #CD7F32; margin-bottom: 100px;">
+            <li style="border-left: 6px solid #CD7F32; margin-bottom: 100px;">
                 <h3><span class="fa fa-trophy" style="color:#CD7F32"></span> {{ $i++ }}</h3>
                 <img src="{{ $item->avatar }}" alt="avatar">
                 <span>{{ $item->first_name }} - {{ number_format($item->totalDistanceWeekly/1000) }} km</span>
