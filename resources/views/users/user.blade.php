f2e66434ab2bb970a782d94ce8d13cbd071bbde1
sorenwagemans <soren.wagemans@hotmail.com>
2017-12-06 09:18:36 +0100

    added progress in user view

--- resources/views/users/user.blade.php
+++ resources/views/users/user.blade.php
@@ -1,14 +1,74 @@
 @extends ('layout.app')
 @section('content')
+
     <section class="users">
         <div class="user">
             <h2>{{$clickedUser->first_name}} {{$clickedUser->last_name}}</h2>
             <img class="avatar" src="{{$clickedUser->avatar}}" alt="avatar">
         </div>
-        <form class="form-add" action="/user/{{$clickedUser->id}}" method="post">
-            {{ csrf_field() }}
-            <input value="{{$clickedUser->id}}" type="hidden" name="friend_id">
-            <button class="add-friend" type="submit">Add as friend</button>
-        </form>
     </section>
+
+    <div class="row">
+        <div class="col-md-3">
+            <div class="trophy-case">
+                <img class="medal medal-full" src="images/medals/platinum.png" alt="">
+                <h3>{{ count($clickedUser->medals->where('type', 'platinum')) }}</h3>
+                <p>Trophies</p>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="trophy-case">
+                <img class="medal medal-full" src="images/medals/gold.png" alt="">
+                <h3>{{ count($clickedUser->medals->where('type', 'gold')) }}</h3>
+                <p>Gold medals</p>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="trophy-case">
+                <img class="medal medal-full" src="images/medals/silver.png" alt="">
+                <h3>{{ count($clickedUser->medals->where('type', 'silver')) }}</h3>
+                <p>Silver medals</p>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="trophy-case">
+                <img class="medal medal-full" src="images/medals/bronze.png" alt="">
+                <h3>{{ count($clickedUser->medals->where('type', 'bronze')) }}</h3>
+                <p>Bronze medals</p>
+            </div>
+        </div>
+    </div>
+    <div class="user-progress">
+        <div class="line" style="left: 25%">
+            <p>25%</p>
+            <p class="bottom"><img class="medal medal-icon" src="images/medals/bronze.png" alt=""></p>
+        </div>
+        <div class="line" style="left: 50%">
+            <p>50%</p>
+            <p class="bottom"><img class="medal medal-icon" src="images/medals/silver.png" alt=""></p>
+        </div>
+        <div class="line" style="left: 95%">
+            <p>100%</p>
+            <p class="bottom"><img class="medal medal-icon" src="images/medals/gold.png" alt=""></p>
+        </div>
+        @if ($goal['totalRanThisWeek'] == 0)
+            <div class="inner" style="width: 0%">
+
+            </div>
+            <div class="progress-info">
+                <p>0 / {{ $goal['goalThisWeek'] }}KM</p>
+                <span>Volgende week: {{ $goal['goalNextWeek'] }}KM lopen</span>
+            </div>
+        @else
+            <div class="inner"
+                 style="width: {{ round((($goal['totalRanThisWeek'] / 1000) / $goal['goalThisWeek']) * 100) }}%">
+
+            </div>
+            <div class="progress-info">
+                <span class="weekly">Weekly Goal:</span>
+                <p>{{ round(($goal['totalRanThisWeek'] / 1000), 2) }} / {{ $goal['goalThisWeek'] }}KM</p>
+                <span>Volgende week: {{ $goal['goalNextWeek'] }}KM lopen</span>
+            </div>
+        @endif
+    </div>
 @endsection
