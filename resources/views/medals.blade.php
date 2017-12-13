51e2e68948702dc076244dff5d4227161e07b0c3
sorenwagemans <soren.wagemans@hotmail.com>
2017-12-13 10:37:11 +0100

    add correct english to medals info

--- resources/views/medals.blade.php
+++ resources/views/medals.blade.php
@@ -3,29 +3,29 @@
 @section('content')
     @if (Auth::user()->medals->isEmpty())
         <div class="no-content">
-            <h3>U have no medals</h3>
+            <h3>You have no medals</h3>
             <h4>How can I earn medals?</h4>
-            <p>By running your weekly goals, u can earn following medals:</p>
+            <p>By running your weekly goals, you can earn following medals:</p>
         </div>
         <div class="row">
             <div class="col-md-4">
                 <div class="trophy-case">
                     <img class="medal medal-full" src="images/medals/gold.png" alt="">
-                    <h3>#</h3>
+                    <h3>0</h3>
                     <p>Gold medals</p>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="trophy-case">
                     <img class="medal medal-full" src="images/medals/silver.png" alt="">
-                    <h3>#</h3>
+                    <h3>0</h3>
                     <p>Silver medals</p>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="trophy-case">
                     <img class="medal medal-full" src="images/medals/bronze.png" alt="">
-                    <h3>#</h3>
+                    <h3>0</h3>
                     <p>Bronze medals</p>
                 </div>
             </div>
